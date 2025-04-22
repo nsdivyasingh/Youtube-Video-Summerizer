569ee5cc6c19fcb27cdcd9dc1e33f107a7ff1ef6
Naveen Singh Divya Singh <157987269+nsdivyasingh@users.noreply.github.com>
2025-04-22 23:27:33 +0530

    resolved issues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
 import google.generativeai as genai
@@ -61,6 +61,3 @@ if video_url:
                 st.write("Could not generate a summary.")
         else:
             st.write("Could not retrieve transcript.")
-=======
-
->>>>>>> 79b97ed7ff24c30678cfaed2e315f92449cdd650
